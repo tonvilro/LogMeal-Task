2bb3361f69bfc7cfc91b9ca3a931343481f71e08
Ton <ton.vila.r@gmail.com>
2023-01-13 11:54:20 +0100

    /analyze_image: Working

--- web.py
+++ web.py
@@ -1,6 +1,8 @@
 import os
+import re
 from datetime import datetime
 from flask import Flask, jsonify, request
+from PIL import Image
 
 app = Flask(__name__)
 ImageFolder = 'Images'
@@ -23,13 +25,30 @@ def upload_image():
         return jsonify({'ERROR': 'No image sent'}), 400
 
     if not allowed_file(image.filename):
-        return jsonify({'ERROR': 'Invalid image extension. Accepted extensions: JPG, JPEG, PNG, GIF'}), 400
+        return jsonify({'ERROR': 'Invalid extension. Accepted image extensions: JPG, JPEG, PNG, GIF'}), 400
 
     # Generate Unique Image ID using actual datetime
-    newId = generate_id(image.filename)
+    image_id = generate_id(image.filename)
 
-    image.save(os.path.join(app.config['UPLOAD_FOLDER'], newId))
-    return jsonify({'SUCCESS': 'We got your image!'}), 200
+    image.save(os.path.join(app.config['UPLOAD_FOLDER'], image_id))
+    return jsonify({'OK': 'We got your image!', 'ID': image_id}), 200
+
+
+@app.route('/analyze_image/<image_id>', methods=['GET'])
+def analyze_image(image_id):
+    try:
+        folder_path = os.path.join(app.config['UPLOAD_FOLDER'])
+        image_path = f"{folder_path}/{image_id}"
+
+        with Image.open(image_path) as img:
+            width, height = img.size
+            return jsonify({'OK': 'Image found!', 'height': height, 'width': width}), 200
+
+    except IOError:
+        return jsonify({'ERROR': f'Image not found: {image_id}'}), 404
+
+    except Exception as e:
+        return jsonify({'ERROR': str(e)}), 500
 
 
 def allowed_file(filename):
@@ -45,6 +64,9 @@ def generate_id(filename):
 
 def get_file_extension(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower()
+
+def remove_file_extension(filename):
+    return '.'.join(filename.rsplit('.')[:-1])
 
 if __name__ == '__main__':
     app.run()
